7db4c2a0dabad0287fc0a27299ee5436445aca42
scoronadovasco <scoronadovasco@gmail.com>
2025-11-04 14:30:27 -0500

    Refactor code structure and remove redundant code blocks for improved readability and maintainability

--- src/AppStore/obj/Debug/net9.0/AppStore.AssemblyInfo.cs
+++ src/AppStore/obj/Debug/net9.0/AppStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5b41a98fc9127ff8c43f2896894d871df274bf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
